cd20703faf2bba69fe906251e7883de8fc14d89e
Jay Snellen, III <jsnellen@jsu.edu>
2023-01-23 13:01:14 -0600

    Added pretty whitespace

--- src/edu/jsu/mcis/cs310/Main.java
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -3,16 +3,12 @@ package edu.jsu.mcis.cs310;
 public class Main {
 
     public static void main(String[] args) {
-        
         Main m = new Main();
         System.out.println(m.getGreeting());
-        
     }
     
     public String getGreeting() {
-        
         return "Hello, World!";
-        
     }
     
 }
